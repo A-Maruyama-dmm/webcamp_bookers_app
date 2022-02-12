b3c553d4648e74e21d20629ffc1a83d607b83b23
A-Maruyama-dmm <com.mac.dm@gmail.com>
2022-02-12 08:29:03 +0000

    create機能

--- app/controllers/bookes_controller.rb
+++ app/controllers/bookes_controller.rb
@@ -12,8 +12,22 @@ class BookesController < ApplicationController
 
   def edit
   end
-  
+
   def create
+   @book = Book.new(books_params)
+    if @book.save
+      redirect_to booke_path(@book)
+      flash[:success] = "Book was successfully created."
+    else
+    @bookes = Book.all
+      render :index
+    end
   end
-  
+
+
+private
+  def books_params
+    params.permit(:title, :body)
+  end
+
 end
